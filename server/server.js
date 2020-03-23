06ae98b6566b1d23d1d5f010dcf962b498fb634b
Jin <jinruncai@gmail.com>
2020-03-23 15:56:55 -0700

    Serve static assets

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
+const path = require('path');
 const express = require('express');
 const proxyServer = express();
+
+proxyServer.use(express.static(
+    path.resolve(__dirname, '..', 'public'),
+));
 
 module.exports = proxyServer;
