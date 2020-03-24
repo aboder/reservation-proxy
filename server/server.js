5290e918ed0eb297b732d241cc55f37237edc608
Jin <jinruncai@gmail.com>
2020-03-24 16:25:25 -0700

    Proxy requests for bundles

--- server/server.js
+++ server/server.js
@@ -6,21 +6,37 @@ const DOMAINS = require('../constants/DOMAINS')
 
 const proxyServer = express();
 
+proxyServer.use('/*bundle.js', (req, res) => {
+    const { service } = req.originalUrl.match(/\/(?<service>.+)bundle.js/i).groups;
+    redirectRequest(req, DOMAINS[service])
+        .then(({data}) => {
+            res.send(data);
+        })
+        .catch((reason) => {
+            console.log(reason);
+            res.status(500).send(reason);
+        });
+});
+
 proxyServer.use(express.static(
     path.resolve(__dirname, '..', 'public'),
 ));
 
 proxyServer.use('/api/reservations', (req, res) => {
-    const reservations = { DOMAINS };
+    const { reservations } = DOMAINS;
+    console.log(reservations);
     redirectRequest(req, reservations)
         .then(({ data }) => {
             res.send(data);
         })
-        .catch(console.err);
+        .catch((reason) => {
+            console.log(reason);
+            res.status(500).send();
+        });
 });
 
 proxyServer.use('/api/reviews', (req, res) => {
-    const reviews = { DOMAINS };
+    const { reviews } = DOMAINS;
     redirectRequest(req, reviews)
         .then(({ data }) => {
             res.send(data);
