9a35e43109add2077076c9da631f03f69b6b662d
Jin <jinruncai@gmail.com>
2020-03-24 16:40:02 -0700

    Add airbnb style eslint

--- server/server.js
+++ server/server.js
@@ -2,46 +2,48 @@ const path = require('path');
 const express = require('express');
 const redirectRequest = require('../utils/redirectRequest');
 
-const DOMAINS = require('../constants/DOMAINS')
+const DOMAINS = require('../constants/DOMAINS');
 
 const proxyServer = express();
 
 proxyServer.use('/*bundle.js', (req, res) => {
-    const { service } = req.originalUrl.match(/\/(?<service>.+)bundle.js/i).groups;
-    redirectRequest(req, DOMAINS[service])
-        .then(({data}) => {
-            res.send(data);
-        })
-        .catch((reason) => {
-            console.log(reason);
-            res.status(500).send(reason);
-        });
+  const { service } = req.originalUrl.match(/\/(?<service>.+)bundle.js/i).groups;
+  redirectRequest(req, DOMAINS[service])
+    .then(({ data }) => {
+      res.send(data);
+    })
+    .catch((reason) => {
+      console.log(reason);
+      res.status(500).send();
+    });
 });
 
 proxyServer.use(express.static(
-    path.resolve(__dirname, '..', 'public'),
+  path.resolve(__dirname, '..', 'public'),
 ));
 
 proxyServer.use('/api/reservations', (req, res) => {
-    const { reservations } = DOMAINS;
-    console.log(reservations);
-    redirectRequest(req, reservations)
-        .then(({ data }) => {
-            res.send(data);
-        })
-        .catch((reason) => {
-            console.log(reason);
-            res.status(500).send();
-        });
+  const { reservations } = DOMAINS;
+  redirectRequest(req, reservations)
+    .then(({ data }) => {
+      res.send(data);
+    })
+    .catch((reason) => {
+      console.log(reason);
+      res.status(500).send();
+    });
 });
 
 proxyServer.use('/api/reviews', (req, res) => {
-    const { reviews } = DOMAINS;
-    redirectRequest(req, reviews)
-        .then(({ data }) => {
-            res.send(data);
-        })
-        .catch(console.err);
+  const { reviews } = DOMAINS;
+  redirectRequest(req, reviews)
+    .then(({ data }) => {
+      res.send(data);
+    })
+    .catch((reason) => {
+      console.log(reason);
+      res.status(500).send();
+    });
 });
 
 module.exports = proxyServer;
